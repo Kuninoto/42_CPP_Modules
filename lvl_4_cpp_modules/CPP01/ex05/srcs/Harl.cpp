c74d9519925b7e0ea1f0de5323f9655a3d75de02
kuninoto <nnuno-ca@student.42porto.com>
2023-02-04 00:13:22 +0000

    [CPP01 ex06] Done

--- lvl_4_cpp_modules/CPP01/ex05/srcs/Harl.cpp
+++ lvl_4_cpp_modules/CPP01/ex05/srcs/Harl.cpp
@@ -6,13 +6,12 @@ using std::cout;
 using std::endl;
 
 #define HARL_DEBUG  "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
-#define HARL_INFO  "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
-#define HARL_WARNING  "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month."
+#define HARL_INFO  "I cannot believe adding extra bacon costs more money.\nYou didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+#define HARL_WARNING  "I think I deserve to have some extra bacon for free.\nI've been coming for years whereas you started working here since last month."
 #define HARL_ERROR  "This is unacceptable! I want to speak to the manager now."
 
 Harl::Harl(void) {};
 Harl::~Harl(void) {};
-
 
 void Harl::debug(void) {
 	cout << HARL_DEBUG << endl;
