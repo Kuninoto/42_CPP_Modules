f6571cb832ba90a14bca50a421538d4351662a59
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r13s12.42porto.com>
2023-02-07 19:37:33 +0000

    [CPP03 ex01] small update

---
+++ lvl_4_cpp_modules/CPP03/ex01/includes/ScavTrap.hpp
@@ -0,0 +1,15 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+class ScavTrap
+{
+	public:
+		ScavTrap(void);
+		~ScavTrap();
+
+	private:
+
+};
+
+
+#endif
--- lvl_4_cpp_modules/CPP03/ex01/main.cpp
+++ lvl_4_cpp_modules/CPP03/ex01/main.cpp
@@ -7,22 +7,6 @@ using std::endl;
 
 int main(void)
 {
-	ClapTrap Jubileus("Jubileus");
-	ClapTrap Robert("Robert");
-
-	Jubileus.attack("Robert");
-	Robert.takeDamage(0);
-
-	Jubileus.attack("Robert");
-	Robert.takeDamage(0);
-
-	Jubileus.attack("Robert");
-	Robert.takeDamage(0);
-
-	Robert.beRepaired(0);
-
-	Jubileus.attack("Robert");
-	Robert.takeDamage(0);
 
 	return EXIT_SUCCESS;
 }
