abf65e4b32e78c1b4577fa810c8ef60684d47e06
kuninoto <nnuno-ca@student.42porto.com>
2023-02-21 00:43:54 +0000

    [CPP05 ex00] Done!

--- lvl_4_cpp_modules/CPP05/ex00/includes/Bureaucrat.hpp
+++ lvl_4_cpp_modules/CPP05/ex00/includes/Bureaucrat.hpp
@@ -1,22 +1,45 @@
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+# include <exception>
+# include <iostream>
 # include <string>
 using std::string;
 
-class BureauCrat {
+# define HIGHEST_GRADE 1
+# define LOWEST_GRADE 150
+
+class Bureaucrat {
 	public:
-		BureauCrat();
+		Bureaucrat(void);
+		Bureaucrat(string name, int grade);
+		Bureaucrat &operator=(const Bureaucrat &original);
+		~Bureaucrat(void);
 
-		string const getName();
-		size_t getGrade();
+		string const getName(void);
+		int getGrade(void);
 
-		void incrementGrade();
-		void decrementGrade();
+		void setGrade(int grade);
+
+		void incrementGrade(void);
+		void decrementGrade(void);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
 
 	private:
 		string const name;
-		size_t grade;
+		int grade;
 };
+
+std::ostream &operator<<(std::ostream &stream, Bureaucrat &Bureaucrat);
 
 #endif // BUREAUCRAT_HPP
--- lvl_4_cpp_modules/CPP05/ex00/main.cpp
+++ lvl_4_cpp_modules/CPP05/ex00/main.cpp
@@ -1,7 +1,100 @@
 #include <cstdlib>
 #include "Bureaucrat.hpp"
 
+using std::cout;
+using std::cerr;
+using std::endl;
+
 int main(void)
 {
+	cout << "GENERAL" << endl;
+	{
+		// Constructor
+		Bureaucrat	a("Ana", 87);
+		// Copy Constructor
+		Bureaucrat	b(a);
+		// Copy assignment
+		Bureaucrat	c = b;
+
+		// ostream overload
+		cout << "a: " << a << endl;
+		cout << "b: " << b << endl;
+		cout << "c: " << c << endl;
+	}
+
+	cout << endl;
+
+	{
+		cout << "TEST 1" << endl;
+		try
+		{
+			// grade too low
+			Bureaucrat	a("A", LOWEST_GRADE + 1);
+			cout << a << endl;
+		}
+		catch (std::exception& e) {
+			cerr << e.what() << endl;
+		}
+
+		cout << endl;
+
+		cout << "TEST 2" << endl;
+		try
+		{
+			// grade too high
+			Bureaucrat	b("B", -1);
+			cout << b << endl;
+		}
+		catch (std::exception& e) {
+			cerr << e.what() << endl;
+		}
+
+		cout << endl;
+
+		cout << "TEST 3" << endl;
+		try
+		{
+			// Increment to a grade higher than HIGHEST_GRADE
+			Bureaucrat	c("C", HIGHEST_GRADE);
+
+			c.incrementGrade();
+			cout << c << endl;
+		}
+		catch (std::exception& e) {
+			cerr << e.what() << endl;
+		}
+
+		cout << endl;
+
+		cout << "TEST 4" << endl;
+		try
+		{
+			// Decrement to a grade lower than LOWEST_GRADE
+			Bureaucrat	d("D", LOWEST_GRADE);
+
+			d.decrementGrade();
+			cout << d << endl;
+		}
+		catch (std::exception& e) {
+			cerr << e.what() << endl;
+		}
+
+		cout << endl;
+
+		cout << "TEST 5" << endl;
+		try 
+		{
+			// Acceptable grade changes
+			Bureaucrat	ebil("EBIL", 10);
+
+			ebil.decrementGrade();
+			cout << ebil << endl;
+			ebil.incrementGrade();
+			cout << ebil << endl;
+		}
+		catch (std::exception& e) {
+			cerr << e.what() << endl;
+		}
+	}
 	return EXIT_SUCCESS;
 }
