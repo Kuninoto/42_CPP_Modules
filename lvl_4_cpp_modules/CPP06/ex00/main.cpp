e83f3719403e4a7bd320a49aff8f77ef922f242f
kuninoto <nnuno-ca@student.42porto.com>
2023-03-22 12:50:50 +0000

    [CPP06 ex00] Trying to make sense out of these subject

--- lvl_4_cpp_modules/CPP06/ex00/main.cpp
+++ lvl_4_cpp_modules/CPP06/ex00/main.cpp
@@ -1,17 +1,17 @@
-#include "ScalarConverter.hpp"
 #include <iostream>
 #include <cstdlib>
+#include "ScalarConverter.hpp"
 
 using std::cerr;
 using std::endl;
 
 int	main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 2 || !argv[1][1])
     {
         cerr << "Usage: ./convert <literal>" << endl;
         return EXIT_FAILURE;
     }
-    
+    ScalarConverter::convert(argv[1]);
     return EXIT_SUCCESS;
 }
