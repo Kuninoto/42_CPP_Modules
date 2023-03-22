e86a5f83e7e2e2a9ea6dd075e0ac090990165b6c
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r14s14.42porto.com>
2023-03-22 19:11:07 +0000

    [CPP06 ex01] Done

--- lvl_4_cpp_modules/CPP06/ex00/includes/ScalarConverter.hpp
+++ lvl_4_cpp_modules/CPP06/ex00/includes/ScalarConverter.hpp
@@ -7,11 +7,6 @@ using std::string;
 class ScalarConverter {
 	public:
 		static void convert(string literal);
-	private:
-		static string as_char(string& literal);
-		static string as_int(string& literal);
-		static string as_float(string& literal);
-		static string as_double(string& literal);
 };
 
 #endif // SCALARCONVERTER_HPP
