ee725eb4f25a62e793de6e20d6336ae7bdcd3101
kuninoto <nnuno-ca@student.42porto.com>
2023-03-22 12:50:50 +0000

    [CPP06 ex00] Trying to make sense out of these subject

--- lvl_4_cpp_modules/CPP06/ex00/includes/ScalarConverter.hpp
+++ lvl_4_cpp_modules/CPP06/ex00/includes/ScalarConverter.hpp
@@ -6,12 +6,12 @@ using std::string;
 
 class ScalarConverter {
 	public:
-		void convert(string literal);
+		static void convert(string literal);
 	private:
-		string as_char(string& literal);
-		string as_int(string& literal);
-		string as_float(string& literal);
-		string as_double(string& literal);
+		static string as_char(string& literal);
+		static string as_int(string& literal);
+		static string as_float(string& literal);
+		static string as_double(string& literal);
 };
 
 #endif // SCALARCONVERTER_HPP
--- lvl_4_cpp_modules/CPP06/ex00/srcs/ScalarConverter.cpp
+++ lvl_4_cpp_modules/CPP06/ex00/srcs/ScalarConverter.cpp
@@ -1,46 +1,69 @@
 #include <iostream>
 #include <limits>
+#include <sstream>
 #include "ScalarConverter.hpp"
 
 using std::cout;
 using std::endl;
 
-void ScalarConverter::convert(string literal)
+/* UTILS */
+
+static int ft_stoi(string& str)
 {
-    cout << "char: " << as_char(literal) << endl;
-    cout << "int: " << as_int(literal) << endl;
-    cout << "float: " << as_float(literal) << endl;
-    cout << "double: " << as_double(literal) << endl;
+    int num;
+    std::stringstream ss(str);
+
+    ss >> num;
+    return num;
 }
 
-/* string ScalarConverter::as_char(string& literal)
+static bool is_pseudo_literal(string& literal) {
+    return (literal == "-inf" || literal == "-inff"
+         || literal == "+inf" || literal == "+inff"
+         || literal == "nan");
+}
+
+void ScalarConverter::convert(string literal)
 {
-    try {
-        return (static_cast<char>(literal));
-    }
-    catch (const std::exception& e) {
-        return ("Impossible");
-    }
-} */
+    cout << "char: '" << as_char(literal) << "'" << endl;
+    cout << "int: " << as_int(literal) << endl;
+//  cout << "float: " << as_float(literal) << endl;
+//  cout << "double: " << as_double(literal) << endl;
+}
+
+string ScalarConverter::as_char(string& literal)
+{
+    int decimal_value;
+
+    decimal_value = ft_stoi(literal);
+    if (decimal_value < std::numeric_limits<char>::min()
+    ||  decimal_value > std::numeric_limits<char>::max())
+        return string("impossible");
+    if (isprint(decimal_value))
+        return string(1, decimal_value);
+    else
+        return string("Non displayable");
+}
 
 string ScalarConverter::as_int(string& literal)
 {
-    try {
-        double n = std::stoi(literal);
-        return (literal);
-    }
-    catch (const std::exception& e) {
-        return ("Impossible");
-    }
+    for (size_t i = 0; i < literal.length(); i += 1)
+        if (!isdigit(literal.at(i))
+        && (literal.at(i) == '+' && literal.at(i) == '-'))
+            return string("impossible");
+    return string(literal);
 }
 
+/*
+string ScalarConverter::as_float(string& literal)
+{
+
+}
+*/
+
+/*
 string ScalarConverter::as_double(string& literal)
 {
-    try {
-        double n = std::stod(literal);
-        return (literal);
-    }
-    catch (const std::exception& e) {
-        return ("Impossible");
-    }
+
 }
+*/
