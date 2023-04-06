c03c1242b06c59d894294034358bdd5c7c502473
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r14s8.42porto.com>
2023-04-06 22:45:45 +0100

    [CPP09] ex01 done

--- lvl_4_cpp_modules/CPP09/ex00/includes/BitcoinExchange.hpp
+++ lvl_4_cpp_modules/CPP09/ex00/includes/BitcoinExchange.hpp
@@ -1,15 +1,66 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <string>
+# include <map>
+
+struct date_t
+{
+    unsigned int year;
+    unsigned int month;
+    unsigned int day;
+
+    bool operator==(const date_t &date) const {
+        return year == date.year && month == date.month && day == date.day;
+    }
+
+    // oldest date
+    bool operator<(const date_t &date)  const
+    {
+        if (year < date.year)
+            return true;
+        else if (year > date.year)
+            return false;
+        else // year is equal
+        {
+            if (month < date.month)
+                return true;
+            else if (month > date.month)
+                return false;
+            else // year and month are equal 
+                return day < date.day;
+        }
+    }
+
+    // most recent date
+    bool operator>(const date_t &date) const
+    {
+        if (year > date.year)
+            return true;
+        else if (year < date.year)
+            return false;
+        else // year is equal
+        {
+            if (month > date.month)
+                return true;
+            else if (month < date.month)
+                return false;
+            else // year and month are equal
+                return day > date.day;
+        }
+    }
+};
+
 class BitcoinExchange {
     public:
         BitcoinExchange(void);
-        BitcoinExchange(const BitcoinExchange &to_copy);
-        BitcoinExchange &operator=(const BitcoinExchange& to_copy);
         ~BitcoinExchange(void);
 
-    private:
+        float getRateFromDataBase(std::string date);
 
+    private:
+        std::map<std::string, float> dataBase;
+        void readDataBase();
 };
 
 #endif // BITCOINEXCHANGE_HPP
--- lvl_4_cpp_modules/CPP09/ex00/srcs/BitcoinExchange.cpp
+++ lvl_4_cpp_modules/CPP09/ex00/srcs/BitcoinExchange.cpp
@@ -1,26 +1,84 @@
-#include <exception>
-#include <stdexcept>
-#include <algorithm>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "BitcoinExchange.hpp"
 
-using std::cout;
-using std::endl;
+#define DB_FILE "./data.csv"
 
-BitcoinExchange::BitcoinExchange(void) {};
-
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &to_copy) {
-	if (this != &to_copy)
-		*this = to_copy;
-}
-
-BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &to_copy)
+static unsigned int ft_stou(const std::string& str)
 {
-	if (this == &to_copy)
-		return (*this);
-	this->max_size = to_copy.max_size;
-	this->storage = to_copy.storage;
-	return (*this);
+    unsigned int num;
+    std::stringstream ss(str);
+
+    ss >> num;
+    return num;
 }
+
+static float ft_stof(const std::string& str)
+{
+    float num;
+    std::stringstream ss(str);
+
+    ss >> num;
+    return num;
+}
+
+BitcoinExchange::BitcoinExchange(void) {
+    this->readDataBase();
+};
 
 BitcoinExchange::~BitcoinExchange(void) {};
+
+date_t parseDate(const std::string& date)
+{
+    size_t first_delim = date.find('-');
+    size_t second_delim = date.find('-', first_delim + 1);
+    
+    return (date_t){ .year = ft_stou(date.substr(0, first_delim)),
+                     .month = ft_stou(date.substr(first_delim + 1, second_delim)),
+                     .day = ft_stou(date.substr(second_delim + 1))
+                   };
+}
+
+static float getRateFromPreviousDate(std::map<std::string, float>& db, std::string date_to_search)
+{
+    std::map<std::string, float>::iterator itr;
+
+    // iterate the map until a bigger date is found
+    // then return the date immediatly before that
+    for (itr = db.begin(); itr != db.end(); itr++)
+    {
+        if (parseDate(itr->first) > parseDate(date_to_search))
+        {
+            itr--;
+            return itr->second;
+        }
+    }
+    return 0.0f;
+}
+
+void BitcoinExchange::readDataBase(void)
+{
+    std::ifstream db(DB_FILE, std::ifstream::in);
+    std::string line;
+    size_t delim;
+
+    // skip first line
+    getline(db, line);
+    while (std::getline(db, line))
+    {
+        delim = line.find(',');
+        // set a new pair on the map <date, rate>
+        std::string rate = line.substr(delim + 1, line.length());
+        this->dataBase[line.substr(0, delim)] = ft_stof(rate);
+    }
+    db.close();
+}
+
+float BitcoinExchange::getRateFromDataBase(std::string date)
+{
+    if (this->dataBase.count(date) > 0)
+        return this->dataBase.at(date);
+    return getRateFromPreviousDate(this->dataBase, date);
+}
