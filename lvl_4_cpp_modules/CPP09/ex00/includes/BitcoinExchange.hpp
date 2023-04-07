f20c445de816e0fdf1e62dd1d922b30de8507e74
Kuninoto <miguelrebelo9999@gmail.com>
2023-04-07 14:51:05 +0100

    [CPP09] ex00 done

--- lvl_4_cpp_modules/CPP09/ex00/includes/BitcoinExchange.hpp
+++ lvl_4_cpp_modules/CPP09/ex00/includes/BitcoinExchange.hpp
@@ -4,59 +4,16 @@
 # include <string>
 # include <map>
 
-struct date_t
-{
-    unsigned int year;
-    unsigned int month;
-    unsigned int day;
-
-    bool operator==(const date_t &date) const {
-        return year == date.year && month == date.month && day == date.day;
-    }
-
-    // oldest date
-    bool operator<(const date_t &date)  const
-    {
-        if (year < date.year)
-            return true;
-        else if (year > date.year)
-            return false;
-        else // year is equal
-        {
-            if (month < date.month)
-                return true;
-            else if (month > date.month)
-                return false;
-            else // year and month are equal 
-                return day < date.day;
-        }
-    }
-
-    // most recent date
-    bool operator>(const date_t &date) const
-    {
-        if (year > date.year)
-            return true;
-        else if (year < date.year)
-            return false;
-        else // year is equal
-        {
-            if (month > date.month)
-                return true;
-            else if (month < date.month)
-                return false;
-            else // year and month are equal
-                return day > date.day;
-        }
-    }
-};
-
 class BitcoinExchange {
     public:
         BitcoinExchange(void);
         ~BitcoinExchange(void);
 
         float getRateFromDataBase(const std::string& date);
+
+        bool isDateInCorrectFormat(const std::string &date);
+        bool isValidDate(const std::string& date);
+        bool isRateInCorrectFormat(const std::string& rate);
 
     private:
         std::map<std::string, float> dataBase;
